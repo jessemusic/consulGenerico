90eca25cce9f2e7143d26b4a6d7a314ba9c02770
jessemusic <jmc_cursos@hotmail.com>
2021-10-01 01:17:13 -0300

    customizando validações

--- consultoria/consul/src/main/java/br/com/mattec/consul/controller/exceptions/ValidaExceptionHandler.java
+++ consultoria/consul/src/main/java/br/com/mattec/consul/controller/exceptions/ValidaExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,5 +24,20 @@ public class ValidaExceptionHandler {
 		StandardError err = new StandardError(Instant.now(),status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> Validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		
+		String error = "Erro no sistema!";
 
+		ValidationError err = new ValidationError(Instant.now(),HttpStatus.BAD_REQUEST.value(), "Erro no sistema!", "Erro de Validação!", request.getRequestURI());
+		for(FieldError erros : e.getBindingResult().getFieldErrors()){
+			err.addError(erros.getField(), erros.getDefaultMessage());
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	
+	
+	
 }
